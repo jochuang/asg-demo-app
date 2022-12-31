c58cf1563a5f89d3583f2b2fd4ff0a4235a89c44
Jo Huang <joh0324@gmail.com>
2022-12-31 16:56:10 -0500

    modified the /ipaddress endpoint to read ip address from text file

--- server/server.js
+++ server/server.js
@@ -1,24 +1,34 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
 const port = 3000;
 
 // serves the static files
 app.use(express.static(path.join(__dirname, '../client')));
 
 // get IP address route
-app.get('/ipaddress', (req, res) => {
-    return res.send('12345')
-})
+app.get('/ipaddress', (req, res, next) => {
+    try {
+        const ip = fs.readFileSync(path.join(__dirname,'../test.txt'), 'utf8');
+        res.locals.ip = ip;
+    } catch (err) {
+        console.log(err);
+    }
+    return next();
+},
+(req, res) => {
+    return res.json(res.locals.ip);
+});
 
 // health route
 app.get('/health', (req, res) => res.sendStatus(200));
 
 // serves the index.html
 app.use('/', (req, res) => {
-    return res.sendFile(path.join(__dirname, '../client/index.html'))
+    return res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
 app.listen(port, () => {
-    console.log(`Demo app is listing on port ${port}`)
-})
+    console.log(`Demo app is listing on port ${port}`);
+});
